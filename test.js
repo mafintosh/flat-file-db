cbd40cde28fb9d7d00acacb098907072b399ec42
Mathias Buus <mathiasbuus@gmail.com>
2013-12-13 00:57:18 +0100

    basic tests

---
+++ test.js
@@ -0,0 +1,80 @@
+var test = require('tap').test;
+var fs = require('fs');
+var ff = require('./index');
+var os = require('os');
+var path = require('path');
+
+var reset = function(file) {
+	try {
+		fs.unlinkSync(file);
+	} catch (err) {}
+	return file;
+};
+
+var TMP = path.join(os.tmpDir(), 'test1.db');
+
+test('open + write + get', function(t) {
+	t.plan(2);
+
+	var db = ff.sync(reset(TMP));
+
+	db.put('hello', 'world');
+	t.same(db.get('hello'), 'world');
+	db.put('hello-2', 'world-2', function(err) {
+		if (err) throw err;
+		var db2 = ff.sync(TMP);
+		t.same(db2.get('hello-2'), 'world-2');
+	});
+});
+
+test('del', function(t) {
+	t.plan(5);
+
+	var db = ff.sync(reset(TMP));
+
+	db.put('hello', 'world', function(err) {
+		if (err) throw err;
+		t.same(db.get('hello'), 'world');
+		db.del('hello', function(err) {
+			if (err) throw err;
+			var db2 = ff.sync(TMP);
+			t.same(db2.get('hello'), undefined);
+			t.same(db2.keys().length, 0);
+		});
+		t.same(db.get('hello'), undefined);
+		t.same(db.keys().length, 0);
+	});
+});
+
+test('multiple writes', function(t) {
+	t.plan(20);
+
+	var db = ff.sync(reset(TMP));
+
+	for (var i = 0; i < 20; i++) {
+		db.put('hello-'+i, 'world-'+i);
+	}
+
+	db.on('drain', function() {
+		var db2 = ff.sync(TMP);
+
+		for (var i = 0; i < 20; i++) {
+			t.same(db2.get('hello-'+i), 'world-'+i);
+		}
+	});
+});
+
+test('last write wins', function(t) {
+	t.plan(1);
+
+	var db = ff.sync(reset(TMP));
+
+	for (var i = 0; i < 20; i++) {
+		db.put('count', {count:i});
+	}
+
+	db.on('drain', function() {
+		var db2 = ff.sync(TMP);
+		t.same(db2.get('count'), {count:19});
+	});
+})
